c62e22031326febcbc1a2383e40d299541bc89a1
Daniel Tan <122583737+idiotsandwich714@users.noreply.github.com>
2023-02-13 08:53:54 -0800

    Implement drawing feature and allow user to set grid dimensions

--- javascript.js
+++ javascript.js
@@ -1,13 +1,37 @@
 const container = document.getElementById('gridContainer');
+const btn = document.getElementById('sizePrompt');
 
-function createGrid(dimensions) {
-    container.style.setProperty('--grid-rows', dimensions);
-    container.style.setProperty('--grid-cols', dimensions);
-    for (let i = 0; i < (dimensions ** 2); i++) {
-        const gridSquare = document.createElement('div');
-        gridSquare.classList.add('gridSquare');
-        container.appendChild(gridSquare);
+btn.addEventListener('click', () => clearGrid());
+btn.addEventListener('click', () => createGrid());
+
+function createGrid() {
+    let dimensions = prompt("Enter Desired Square-Pixelage (Maximum: 100)", "");
+    
+    if (dimensions > 100) {
+        alert("You must enter a positive integer");
+    } else if (dimensions < 0) {
+        alert("You must enter a positive integer");
+    } else if (isNaN(dimensions)) {
+        alert("You must enter a positive integer");
+    } else {
+        container.style.setProperty('--grid-rows', dimensions);
+        container.style.setProperty('--grid-cols', dimensions);
+        for (let i = 0; i < (dimensions * dimensions); i++) {
+            let gridSquare = document.createElement('div');
+            gridSquare.classList.add('gridSquare');
+            gridSquare.addEventListener('mouseover', draw);
+            container.appendChild(gridSquare);
+        }
     }
 }
 
-createGrid(16);
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function draw(e) {
+    e.target.style.backgroundColor = 'grey';
+}
+
